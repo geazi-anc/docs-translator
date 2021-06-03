50223541ec27625f9cb39ec965c3ac4584185582
GeaziAntunes <geaziac@hotmail.com>
2021-06-03 19:02:46 -0300

    added comments in source code

--- src/app.py
+++ src/app.py
@@ -20,7 +20,8 @@ def translate(filename):
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(10)
 
-    # access google translate
+    # acessa o google tradutor
+    # a URL do site já está configurada para que o texto de origem seja inglês e o texto de saída seja português
     driver.get(
         "https://translate.google.com.br/?hl=pt-BR&tab=wT&sl=en&tl=pt&op=docs")
 
@@ -28,17 +29,18 @@ def translate(filename):
     input_file = driver.find_element_by_name("file")
     input_file.send_keys(str(filename))
 
-    # clica em traduzir
+    # clica no botão traduzir
     driver.find_element_by_xpath(
         '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[2]/c-wiz/div/form/div[2]/div[2]/button/span').click()
 
     # pega o texto que foi traduzido
     text = driver.find_element_by_tag_name("pre").text
 
-    # finish driver
+    # finalizando o driver do chrome
     driver.quit()
 
-    # save the text into a file
+
+    # por fim, salva o texto traduzido num arquivo TXT, dentro do diretório output
     filename = output_dir/filename.name
 
     with open(filename, "w", encoding="utf-8") as outputfile:
@@ -51,6 +53,6 @@ input_dir.mkdir(exist_ok=True)
 output_dir.mkdir(exist_ok=True)
 
 
-# percorre o diretório input
+# percorre o diretório input onde estão os arquivos para serem traduzidos, e cria uma thread individual para cada tradução
 for file in input_dir.iterdir():
     Thread(target=translate, kwargs={"filename": file}).start()
