1cd729c68568e763689769434ad2d0addd930ca7
GeaziAntunes <geaziac@hotmail.com>
2021-06-06 14:00:10 -0300

    new changes: add option to translate a big single file

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 import sys
+import threading
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from threading import Thread
 from pathlib import Path
+from datetime import datetime
 
 
 ### variables ###
@@ -13,13 +15,17 @@ output_dir = Path.cwd().parent/"output"
 ### FUNCTIONS ###
 def translate(filename):
 
+    # datetime and output filename
+    current_time = datetime.now().strftime("%H%M%S%f")
+    output_file = output_dir/f"{current_time}.txt"
+
     # set chrome options
     chrome_options = Options()
-    chrome_options.headless = True
+#    chrome_options.headless = True
 
     # create driver
     driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(60)
 
     # acessa o google tradutor
     # a URL do site já está configurada para que o texto de origem seja inglês e o texto de saída seja português
@@ -31,6 +37,7 @@ def translate(filename):
     input_file.send_keys(str(filename))
 
     # clica no botão traduzir
+    time.sleep(6000)
     driver.find_element_by_xpath(
         '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/div[3]/c-wiz/div[2]/c-wiz/div/form/div[2]/div[2]/button/span').click()
 
@@ -41,9 +48,7 @@ def translate(filename):
     driver.quit()
 
     # por fim, salva o texto traduzido num arquivo TXT, dentro do diretório output
-    filename = output_dir/filename.name
-
-    with open(filename, "w", encoding="utf-8") as outputfile:
+    with open(output_file, "w", encoding="utf-8") as outputfile:
         outputfile.write(text)
 
 
@@ -57,7 +62,8 @@ def strsplit(chars, qty):
     return charslist
 
 
-def get_arg(args):
+def get_args(args):
+
     if len(args) != 2:
         raise IndexError("Requires only one argument: docs or single")
 
@@ -67,16 +73,43 @@ def get_arg(args):
     if arg not in ("docs", "single"):
         raise ValueError("Argument must be docs or single")
 
-    if arg == "single":
-        inputfile = input_dir/"input.txt"
+    return "single" if arg == "single" else "docs"
 
-        with open(inputfile, "r", encoding="utf-8") as file:
-            input = file.read()
-            inputs = strsplit(input, 30000)
 
-    inputfile.unlink()
-    for input in inputs:
-        pass
+def concat():
+    output_file = output_dir/"output.txt"
+    output_texts = ""
+
+    for file in output_dir.iterdir():
+
+        with open(file, "r", encoding="utf-8") as input_file:
+            output_texts += input_file.read()
+
+#        file.unlink()
+
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.write(output_texts)
+
+
+def mkfiles(type_of_translation):
+    input_file = input_dir/"input.txt"
+
+    if type_of_translation != "single":
+        return
+
+    with open(input_file, "r", encoding="utf-8") as file:
+        text = file.read()
+        textslist = strsplit(text, 30000)
+
+    # remove o arquivo input
+    input_file.unlink()
+
+    for inputtext in textslist:
+        current_time = datetime.now().strftime("%H%M%S%f")
+        input_file = input_dir/f"{current_time}.txt"
+
+        with open(input_file, "w", encoding="utf-8") as file:
+            file.write(inputtext)
 
 
 ### MAIN ###
@@ -85,6 +118,19 @@ input_dir.mkdir(exist_ok=True)
 output_dir.mkdir(exist_ok=True)
 
 
+# pega o tipo de tradução
+type_of_translation = get_args(sys.argv)
+mkfiles(type_of_translation)
+
+
 # percorre o diretório input onde estão os arquivos para serem traduzidos, e cria uma thread individual para cada tradução
 for file in input_dir.iterdir():
-    Thread(target=translate, kwargs={"filename": file}).start()
+    threading.Thread(target=translate, kwargs={"filename": file}).start()
+
+# observa as threads em andamento
+while threading.active_count() > 1:
+    print(f"threads ativas: {threading.active_count()}")
+    time.sleep(1)
+
+# concatena os arquivos num único arquivo
+concat()
